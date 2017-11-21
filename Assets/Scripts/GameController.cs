36192786a0e2f367000936cd7d89b59c854ceda5
Ira Fay <ira@faygames.com>
2017-11-21 12:54:25 -0500

    Created the grid and randomly colored next cube

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+	public GameObject cubePrefab;
+	float gameLength = 60;
+	int gridX = 8;
+	int gridY = 5;
+	GameObject[,] grid;
+	GameObject nextCube;
+	Vector3 cubePos;
+	Vector3 nextCubePos = new Vector3 (7, 10, 0);
+	float turnLength = 2;
+	int turn = 0;
+	Color[] myColors = { Color.blue, Color.red, Color.green, Color.yellow, Color.magenta };
+
+	void CreateGrid () {
+		grid = new GameObject[gridX, gridY];
+
+		for (int y = 0; y < gridY; y++) {
+			for (int x = 0; x < gridX; x++) {
+				cubePos = new Vector3 (x * 2, y * 2, 0);
+				Instantiate (cubePrefab, cubePos, Quaternion.identity);
+			}
+		}
+
+	}
+
+	void CreateNextCube () {
+		nextCube = Instantiate (cubePrefab, nextCubePos, Quaternion.identity);
+		nextCube.GetComponent<Renderer> ().material.color = myColors [ Random.Range(0, myColors.Length) ];
+	}
 
 	// Use this for initialization
 	void Start () {
+
+		CreateGrid ();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Time.time > turnLength * turn) {
+			turn++;
+
+			CreateNextCube ();
+
+		}
+
 		
 	}
 }
