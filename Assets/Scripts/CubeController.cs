d360663c60a0b5dd7215dd38c85e8e0e908d20bf
Ira Fay <ira@faygames.com>
2017-11-21 12:23:52 -0500

    Created setup for the project from scratch

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeController : MonoBehaviour {
-	GameController myGameController;
-	public int myX, myY;
 
 	// Use this for initialization
 	void Start () {
-		myGameController = GameObject.Find ("GameControllerObject").GetComponent<GameController> ();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-
-	void OnMouseDown () {
-		myGameController.ProcessClick (gameObject, myX, myY);
 	}
 }
